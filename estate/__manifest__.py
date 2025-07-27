c5b6a24ae919dac73415a666d056edf0c1def690
David Ferreras <ferrerasldf@hotmail.com>
2025-07-27 00:45:47 -0400

    1 to 3 chapters done

---
+++ estate/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'version': '1.2',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'Track leads and close opportunities',
+    'description': "",
+    'website': 'https://www.odoo.com/page/crm',
+    'depends': [
+        'base_setup',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
