fc0cfda222abf649e537237fbf8f1d5356638722
David Ferreras <ferrerasldf@hotmail.com>
2025-08-05 16:52:00 -0400

    Chapter 5
    I have to check the copy parameter to avoid duplicated Available Dates

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,7 +13,9 @@
         'base_setup',
     ],
     'data': [
-
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'demo': [
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,21 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
+from datetime import date
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char('Property Name', required=True)
+    name = fields.Char('Property Name', default="Unknown",required=True)
     description = fields.Text('Property Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Available Date')
+    date_availability = fields.Date('Available Date',
+                                    copy=False,
+                                    default=lambda self: date.today() + relativedelta(months=3))
     expected_price = fields.Float('Expected Price',  required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    selling_price = fields.Float(string='Selling Price', copy=False, readonly=True)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
@@ -21,3 +25,17 @@ class EstateProperty(models.Model):
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    last_seen = fields.Datetime("Last Seen", default= fields.Datetime.now)
+    state = fields.Selection(
+        string='State',
+        required=True,
+        copy=False,
+        selection=[('new', 'New'),
+                   ('offer received', 'Offer Received'),
+                   ('offer accepted', 'Offer Accepted'),
+                   ('sold', 'Sold '),
+                   ('sold', 'Sold '),
+                   ('cancelled', 'Cancelled')],
+        default='new'
+    )
+    active = fields.Boolean(default=True)
